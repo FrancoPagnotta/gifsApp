2b12fe96e268c10345449b0b5063e15cb241128f
Franco Pagnotta <francoapagnotta@gmail.com>
2022-05-13 01:26:45 -0300

    show the list of gifs that match with the search

--- src/app/gifs/results/results.component.ts
+++ src/app/gifs/results/results.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-results',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultsComponent implements OnInit {
 
-  constructor() { }
+  get results(): any[] {
+    return this.gifsService.results;
+  }
+
+  constructor(private gifsService: GifsService) { }
 
   ngOnInit(): void {
   }
